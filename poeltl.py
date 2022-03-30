afc3ecb6f6fd937d8fda13937e216bb57e74f701
Abdulrezak MOhamed <98439067+RezikEffort@users.noreply.github.com>
2022-03-30 14:56:14 -0400

    Added doc String

--- poeltl.py
+++ poeltl.py
@@ -1,5 +1,12 @@
 # Poeltl Game!
-
 class Poeltl:
+    """ Poeltl board display 
+    Attribubte:
+            game (_type_): _description_
+    """
     def __init__(self, game):
+        """Object in the poeltl
+        Args:
+            game (_type_): _description_
+        """
         self.game = game
